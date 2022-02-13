ce8ae8c1d51222f14ba1ba3be356e5014b7f24af
Khaled Saad <65211124+khaled45690@users.noreply.github.com>
2022-02-13 17:57:17 +0200

    ..

--- frontend/lib/contant/constant.dart
+++ frontend/lib/contant/constant.dart
@@ -1,8 +1,8 @@
-// String serverlUrl = "http://192.168.1.3:4000";
+String serverlUrl = "http://192.168.1.7:4000";
 
-// String serverURL1 = "http://192.168.1.3:4000/";
+String serverURL1 = "http://192.168.1.7:4000/";
 
 
-String serverlUrl = "https://okchatt.herokuapp.com";
-
-String serverURL1 ="https://okchatt.herokuapp.com/";
+// String serverlUrl = "https://okchatt.herokuapp.com";
+//
+// String serverURL1 ="https://okchatt.herokuapp.com/";
